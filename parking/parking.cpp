7f0493a8b2eca5bd0e38c35ffc17b77226b320e3
HullJac <jacobhull55@gmail.com>
2021-11-14 18:59:02 -0600

    one more problem done.

---
+++ parking/parking.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+// Take the largest minus the smallest and double that
+int main() {
+    int numCases;
+    std::cin >> numCases;
+
+    int numStores;
+    for (int i = 0; i < numCases; i++) {
+        std::cin >> numStores;
+        std::vector<int> v(numStores);
+        for (auto& i : v) {
+            std::cin >> i;
+        }
+        
+        std::sort(v.begin(), v.end());
+        int distance = v.back() - v.front();
+        distance = distance * 2;
+        std::cout << distance << std::endl;
+    }
+
+    return 0;
+}
