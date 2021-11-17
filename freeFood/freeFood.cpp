b3400b5b7d98a17c5a6b925deba7f96159da5718
HullJac <jacobhull55@gmail.com>
2021-11-17 13:01:10 -0600

    updated free food

--- freeFood/freeFood.cpp
+++ freeFood/freeFood.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
-#include <set>
+#include <algorithm>
+#include <numeric>
 
+// Use a vector of 365 and check each that has a day on it and accumulate on the array.
+//
 // Add all the numbers of the days that have free food to a set and get the size of the set
 int main() {
-    std::set <int> s;    // The set
-    int total;      // Holds the total of the unique days
+    std::vector<int> days(365);// The vector
     int numRows;    // How many events there are
     int start;      // Start of event
     int end;        // End of the event
@@ -27,11 +29,11 @@ int main() {
             }
        }
        for (int n = start; n <= end; n++) {
-            s.insert(n);
+            days[n-1] = 1;
        }
     }
 
-    std::cout << s.size() << std::endl;
+    std::cout << std::accumulate(days.begin(), days.end(), 0) << std::endl;
     
     return 0;
 }
